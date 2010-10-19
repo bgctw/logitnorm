d1bef1a1e63cf0019c9175670ed0937f06dcef9d
Thomas Wutzler <progtw@arcor.de>
2010-10-19 14:24:26 +0000

    added twSigmaLogitnorm

--- inst/exploreLogitNormal.R
+++ inst/exploreLogitNormal.R
@@ -3,7 +3,7 @@
 # Author: twutz
 ###############################################################################
 
-
+library(ggplot2)
 
 xGrid = c(0+.Machine$double.eps,seq(0,1, length.out=81)[-c(1,81)],1-.Machine$double.eps)
 
@@ -71,7 +71,8 @@ qplot(xGrid,value,data=ds[ds$mu %in% c(0,1),], geom="line", color=sigma, ylab="c
 
 mle=0.8
 mu <- seq(0,logit(mle),length.out=10)[-10]
-sigma2 <- (logit(thetaMLE[,2])-thetaMLE[,1])/(2*thetaMLE[,2]-1)   
+tmp <- twSigmaLogitnorm(mle,mu)
+sigma2 <- (logit(mle)-mu)/(2*mle-1)   
 theta0 <- cbind(mu,sigma=as.numeric(sqrt(sigma2)))
 
 ds <- .calcDensityGrid(theta0,xGrid=xGrid)
@@ -109,3 +110,16 @@ theta <- twCoefLogitnormMLE( mle=mle,quant=0.98,perc=0.99)
 quant2 <- qlogitnorm(0.99,mu=theta[1],sigma=theta[2])
 plot( dlogitnorm(xGrid,mu=theta[1],sigma=theta[2])~xGrid, type="l")
 abline(v=c(mle,quant2),col="gray")
+
+
+
+#------- sigma for the flattest case
+mle=0.9
+mu=seq(0,if(mle<0.5) mle else 1-mle,length.out=10)[-10]
+mu=seq(0,0.25,length.out=10)[-10]
+tmp <- twSigmaLogitnorm(mle,mu)
+sigma2 <- (logit(mle)-mu)/(2*mle-1)   
+theta0 <- cbind(mu,sigma=as.numeric(sqrt(sigma2)))
+ds <- .calcDensityGrid(theta0,xGrid=xGrid)
+qplot(xGrid,value,data=ds, geom="line", color=mu, ylim=c(0.9,1.2) )
+
