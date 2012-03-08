e434b95d7aeda2a4e66256648965488004cee812
Thomas Wutzler <progtw@arcor.de>
2012-03-08 12:33:58 +0000

    minor

--- scratch/nick1202.R
+++ scratch/nick1202.R
@@ -22,7 +22,10 @@ qfunct(5,0.1)
 #[1] 9.933e-01 4.484e-07
 
 
-mean(plogis(rnorm(1000000,5,0.1))) 
+tmp <- plogis(rnorm(1000000,5,0.1))
+c( mean(tmp), var(tmp) )
 # 0.993273		# confirmes the qfunct solution
+
+mean(plogis(rnorm(1000000,5,0.1))) 
 
 qfunct(4,1)
